1f94fdca9e6f84d020aeb5f414789056ba7bc80c
7x <info@se7enx.com>
2024-11-17 05:07:39 -0800

    Added: Added copyright and license documentation to source code repository directory

---
+++ ezinfo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File containing the ezpublishbasic copyright information file
+ *
+ * @copyright Copyright (C) 1999 - 2024 7x. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
+ * @version //autogentag//
+ * @package ezpublishbasic
+ */
+
+class ezpublishbasicInfo
+{
+    public static function info()
+    {
+        return array( 'Name' => "<a href='https://github.com/se7enxweb/ezpublishbasic'>7x eZ Publish Basic</a>",
+                      'Version' => "2.4.0.0",
+                      'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>7x</a>",
+                      'License' => "GNU General Public License v2.0 (or later)",
+                      'info_url' => "https://github.com/se7enxweb/ezpublishbasic"
+                    );
+    }
+}
+
+?>

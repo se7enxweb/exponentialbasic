c19531b8bef190482056c05fc4adb4298b0dad4e
7x <info@se7enx.com>
2025-03-08 17:33:42 +0000

    Updated: Updated Copyright Year

--- kernel/ezinfo.php
+++ kernel/ezinfo.php
@@ -14,7 +14,7 @@ class ezpublishbasicInfo
     {
         return array( 'Name' => "<a href='https://github.com/se7enxweb/ezpublishbasic'>7x eZ Publish Basic</a>",
                       'Version' => "2.4.0.0",
-                      'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>7x</a>",
+                      'Copyright' => "Copyright (C) 1999 - 2025 <a href='https://se7enx.com' title='7x'>7x</a>",
                       'License' => "GNU General Public License v2.0 (or later)",
                       'info_url' => "https://github.com/se7enxweb/ezpublishbasic"
                     );

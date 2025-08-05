71ab5ce5ca972908f80f6be9682a3536706d512d
7x <info@se7enx.com>
2025-08-05 15:45:52 -0700

    Updated: Updated designs to use original stats store url calls (now restored).

--- design/hardwarestore/frame.php
+++ design/hardwarestore/frame.php
@@ -239,7 +239,7 @@ print( "<span>" );
     // create a random string to prevent browser caching.
     $seed = md5( microtime() );
     // callback for storing the stats
-    $imgSrc = $GlobalSiteIni->WWWDir . "/stats/store/rx$seed-" . $REQUEST_URI . "1x1.gif";
+    $imgSrc = $GlobalSiteIni->WWWDir . "/stats/store/rx$seed-" . $REQUEST_URI . "/1x1.gif";
     print( "<img src=\"$imgSrc\" height=\"1\" width=\"1\" border=\"0\" alt=\"\" />" );
 print( "</span>" );
 

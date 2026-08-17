00cbebd5fa9bc5e5146667cae60e1937e5a8d774
7x <info@se7enx.com>
2026-08-17 03:00:33 -0700

    Updated: helloworld sample module to load the site language and use extension translation strings.

--- extension/helloworld/modules/helloworld/user/datasupplier.php
+++ extension/helloworld/modules/helloworld/user/datasupplier.php
@@ -3,8 +3,7 @@
 // extension/helloworld/modules/helloworld/user/datasupplier.php
 //
 // Sample module in an extension. It uses an eZTemplate from the extension
-// design path, falling back to a hard-coded output if the template cannot be
-// loaded.
+// design path, with extension translation strings merged into the template.
 
 $ini = eZINI::instance( 'site.ini' );
 if ( isset( $GlobalSectionIDOverride ) )
@@ -16,12 +15,25 @@ else
     $GlobalSectionID = $ini->variable( 'eZUserMain', 'DefaultSection' );
 }
 
+$Language = $ini->variable( 'site', 'Language' );
+
 $templateDir = eZDesign::file( 'templates/helloworld' );
 if ( $templateDir === false )
     $templateDir = 'design/standard/templates/helloworld';
 
-$t = new eZTemplate( $templateDir, '', '', 'datasupplier.php' );
+$t = new eZTemplate( $templateDir, '', $Language, 'datasupplier.php' );
+$t->setAllStrings();
+
 $t->set_file( 'welcome', 'welcome.tpl' );
-$t->set_var( 'hello', 'Hello from the extension module!' );
-$t->set_var( 'description', 'This page is served by extension/helloworld/modules/helloworld/user/datasupplier.php.' );
+
+if ( isset( $t->TextStrings['strings']['hello'] ) )
+    $t->set_var( 'hello', $t->TextStrings['strings']['hello'] );
+else
+    $t->set_var( 'hello', 'Hello from the extension module!' );
+
+if ( isset( $t->TextStrings['strings']['description'] ) )
+    $t->set_var( 'description', $t->TextStrings['strings']['description'] );
+else
+    $t->set_var( 'description', 'This page is served by extension/helloworld/modules/helloworld/user/datasupplier.php.' );
+
 $t->pparse( 'output', 'welcome' );
